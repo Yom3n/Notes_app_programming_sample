fc32782a2e5e3b2103194eadca49598f48253502
Yom3n <kamilbudnik94@gmail.com>
2023-10-27 16:40:17 +0200

    Finalized NotesFeedPage widget tests

--- test/features/notes/blocs/pages/notes_feed_page_test.dart
+++ test/features/notes/blocs/pages/notes_feed_page_test.dart
@@ -5,6 +5,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:notes_app/core/ui/my_progress_indicator.dart';
 import 'package:notes_app/features/notes/blocs/notes_cubit.dart';
 import 'package:notes_app/features/notes/pages/notes_feed_page.dart';
+import 'package:notes_app/models/note.dart';
 
 class MockNotesCubit extends MockCubit<NotesState> implements NotesCubit {}
 
@@ -14,28 +15,70 @@ void main() {
     notesCubitMock = MockNotesCubit();
   });
 
+  ///Pumps [NotesFeedPage] and emits [notesStatesToEmit] through [NotesCubit]
+  Future<void> arrangeNotesFeedPage(
+      {required WidgetTester tester,
+      required List<NotesState> noteStatesToEmit}) async {
+    whenListen(
+      notesCubitMock,
+      Stream.fromIterable(noteStatesToEmit),
+      initialState: const NotesState(status: NotesStatus.initial),
+    );
+    await tester.pumpWidget(
+      MaterialApp(
+        home: BlocProvider<NotesCubit>.value(
+          value: notesCubitMock,
+          child: const NotesFeedPage(),
+        ),
+      ),
+    );
+    await tester.pump();
+  }
+
   testWidgets(
-    'Test page show loading indicator, and then list of notes',
+    'Test page show loading indicator when status loading is emitted',
     (tester) async {
       // Arrange
-
-      whenListen(
-        notesCubitMock,
-        Stream.fromIterable([const NotesState(status: NotesStatus.loading)]),
-        initialState: const NotesState(status: NotesStatus.initial),
-      );
-      await tester.pumpWidget(
-        MaterialApp(
-          home: BlocProvider<NotesCubit>.value(
-            value: notesCubitMock,
-            child: const NotesFeedPage(),
-          ),
-        ),
-      );
-      await tester.pump();
+      await arrangeNotesFeedPage(
+          tester: tester,
+          noteStatesToEmit: [const NotesState(status: NotesStatus.loading)]);
       // Assert
       final loadingIndicator = find.byType(MyProgressIndicator);
       expect(loadingIndicator, findsOneWidget);
+    },
+  );
+
+  testWidgets(
+    'Test page show notes feed when status Loaded is emitted',
+    (tester) async {
+      // Arrange
+      await arrangeNotesFeedPage(tester: tester, noteStatesToEmit: [
+        const NotesState(status: NotesStatus.loaded, notes: [
+          Note(id: 1, title: 'Title 1', text: 'Text 1'),
+          Note(id: 2, title: 'Title 2', text: 'Text 2')
+        ])
+      ]);
+      // Assert
+      final notesFeed = find.byType(NotesFeedBody);
+      expect(notesFeed, findsOneWidget);
+    },
+  );
+
+  testWidgets(
+    'Test page show error message when status Error is emitted',
+    (tester) async {
+      // Arrange
+      await arrangeNotesFeedPage(tester: tester, noteStatesToEmit: [
+        const NotesState(
+          status: NotesStatus.error,
+          noteErrorData: NoteErrorData(
+            NoteErrorStatus.userDoesNotExist,
+          ),
+        ),
+      ]);
+      // Assert
+      final notesFeed = find.byType(NotesErrorBody);
+      expect(notesFeed, findsOneWidget);
     },
   );
 }
